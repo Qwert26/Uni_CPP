01d26c762577f97808d009440fd80a4f1e9cf607
Qwert26 <qwert268862@gmail.com>
2015-10-30 14:53:44 +0100

    Added next Task for Exercise1
    Also solzed printing Problem in SizePrinter.cpp

--- printsizes/Exercise1/SizePrinter.cpp
+++ printsizes/Exercise1/SizePrinter.cpp
@@ -1,6 +1,15 @@
 #include <stdio.h>
 int main(void) {
-	printf("\nchar "+sizeof(char));
-	printf("\n\n\nshort "+sizeof(short));
+	printf("char %zd\n",sizeof(char));
+	printf("short %zd\n",sizeof(short));
+	printf("int %zd\n",sizeof(int));
+	printf("long %zd\n", sizeof(long));
+	printf("long long %zd\n", sizeof(long long));
+	printf("float %zd\n", sizeof(float));
+	printf("double %zd\n", sizeof(double));
+	printf("long double %zd\n", sizeof(long double));
+	char ptr=0;
+	printf("pointer %zd\n",sizeof(&ptr));
+	//Die Länge eines Pointers ist unabhängig von der Länge des Datentyps, auf die er zeigt, aber nicht von der Prozessorstruktur!
 	return 0;
 }
