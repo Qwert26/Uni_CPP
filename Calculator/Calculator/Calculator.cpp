8f14b3a78f43968fa020c3f23bd0225e7962da66
Qwert26 <qwert268862@gmail.com>
2015-11-08 12:50:24 +0100

    Added Calculator

---
+++ Calculator/Calculator/Calculator.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+//compile with /W3
+#pragma warning(disable:4996)
+int main(void) {
+	int a,b,count;
+	char op;
+	char input[50];
+	while(true) {
+		printf("Enter <int> <op> <int>, single '0' to exit: ");
+		fgets(input,50,stdin);
+		//Wie viele UEbereinstimmung haben wir?
+		count=sscanf(input,"%i %c %i",&a,&op,&b);
+		//Eine UEbereinstimmung und a ist 0: Beenden!
+		if (count==1&&a==0) {
+			printf("Goodbye\n");
+			return 0;
+		} /*Nicht 3 UEbereinstimmungen*/ else if (count!=3) {
+			printf("invalid input\n");
+		} /*Genau 3 UEbereinstimmungen*/ else {
+			switch (op) {
+			case '+':
+				printf("%i %c %i = %i\n",a,op,b,a+b);
+				break;
+			case '-':
+				printf("%i %c %i = %i\n", a, op, b, a - b);
+				break;
+			case '*':
+				printf("%i %c %i = %i\n", a, op, b, a * b);
+				break;
+			case '/':
+				printf("%i %c %i = %i\n", a, op, b, a / b);
+				break;
+			// Illegaler Operator obwohl % ein gültiger wäre...
+			default:
+				printf("invalid input\n");
+			}
+		}
+	}
+}
