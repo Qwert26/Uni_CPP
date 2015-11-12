29a77cf4a9d2ce5874d406b8568e78b63ad70733
Qwert26 <qwert268862@gmail.com>
2015-11-12 16:28:41 +0100

    Bugfix Calculator
    / 0 gelöst

--- Calculator/Calculator/Calculator.cpp
+++ Calculator/Calculator/Calculator.cpp
@@ -4,10 +4,10 @@
 int main(void) {
 	int a,b,count;
 	char op;
-	char input[50];
+	char input[128];
 	while(true) {
 		printf("Enter <int> <op> <int>, single '0' to exit: ");
-		fgets(input,50,stdin);
+		fgets(input,128,stdin);
 		//Wie viele UEbereinstimmung haben wir?
 		count=sscanf(input,"%i %c %i",&a,&op,&b);
 		//Eine UEbereinstimmung und a ist 0: Beenden!
@@ -28,7 +28,11 @@ int main(void) {
 				printf("%i %c %i = %i\n", a, op, b, a * b);
 				break;
 			case '/':
-				printf("%i %c %i = %i\n", a, op, b, a / b);
+				if (b) {
+					printf("%i %c %i = %i\n", a, op, b, a / b);
+				} else {
+					printf("invalid input\n");
+				}
 				break;
 			// Illegaler Operator obwohl % ein gültiger wäre...
 			default:
