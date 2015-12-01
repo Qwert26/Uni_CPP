7299df93cdc48f4209a427fbf7006f59c035bc80
Qwert26 <qwert268862@gmail.com>
2015-12-01 17:05:20 +0100

    Updated TimeTest

--- TimeTest/TimeTest/TimeTest.cpp
+++ TimeTest/TimeTest/TimeTest.cpp
@@ -5,12 +5,17 @@
 #define MEASURETIME(description,repetitions,instructions2test)	StopWatch sw;\
 																sw.start();\
 																for(int i=0;i<repetitions;i++) {instructions2test}\
-																double end=sw.stop();\
-																std::cout<<"My computer requires "<<end<<" seconds to execute "<<description<<" "<<repetitions<<" times.\n";
+																std::cout<<"My computer requires "<<sw.stop()<<" seconds to execute "<<description<<" "<<repetitions<<" times.\n";
 void baseOperation(void);
+void copyAssignmentTest(void);
+void moveAssignmentTest(void);
 int main(void) {
-	//100000 Wiederholung bräuchten ungefähr 20 Minuten!
-	MEASURETIME("Baseoperation",10000,baseOperation();)
+	//100000 Wiederholung bräuchten ungefähr 21 Minuten!
+	{MEASURETIME("Baseoperation", 10000, baseOperation();)}
+	//100000 Wiederholungen bräuchten ungefähr 31 Minuten!
+	{MEASURETIME("Copy Assignment Test", 10000, copyAssignmentTest();)}
+	//100000 Wiederholungen bräuchten ungefähr 21 Minuten!
+	{MEASURETIME("Move Assignment Test",10000,moveAssignmentTest();)}
 	return 0;
 }
 void baseOperation(void) {
@@ -18,4 +23,21 @@ void baseOperation(void) {
 	for (int c=0;c<100000;c++) {
 		s.push(rand());
 	}
+}
+void copyAssignmentTest(void) {
+	Stack s(100000);
+	for (int c = 0; c<100000; c++) {
+		s.push(rand());
+	}
+	Stack s2(s);
+}
+Stack randomStack(void) {
+	Stack s(100000);
+	for (int c = 0; c<100000; c++) {
+		s.push(rand());
+	}
+	return s;
+}
+void moveAssignmentTest(void) {
+	const Stack&& s = randomStack();
 }
