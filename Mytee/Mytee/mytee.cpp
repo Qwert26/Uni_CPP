1b5d196cb85720a2fbbd90e54c941ccd174d8326
Qwert26 <qwert268862@gmail.com>
2016-01-31 13:14:04 +0100

    Update Mytee
    Jetzt auch mit Leerzeichen!

--- Mytee/Mytee/mytee.cpp
+++ Mytee/Mytee/mytee.cpp
@@ -5,15 +5,14 @@
 int main(int length,char**args) {
 	using namespace std;
 	time_t now;
-	time(&now);
-	tm *timeInfo = &tm();
+	tm timeInfo;
 	char prefix[64];
 	ofstream log("mytee.log",ofstream::app);
 	string in;
-	while (std::cin>>in) {
-		log << in;
-		localtime_s(timeInfo, &now);
-		strftime(prefix, 64, "%d.%m.%Y %R:%S", timeInfo);
+	while (getline(cin,in)) {
+		time(&now);
+		localtime_s(&timeInfo, &now);
+		strftime(prefix, 64, "%d.%m.%Y %R:%S",&timeInfo);
 		cout << in << endl;
 		log << prefix << " " << in << endl;
 	}
